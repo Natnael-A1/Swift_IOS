423742d03a300fffb383f5b1baff62779bec0cb2
Natnael <nasmelash@captechconsulting.com>
2024-09-18 10:51:36 -0400

    Initial Commit

---
+++ Swift_UI/Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UIApp.swift
+//  Swift_UI
+//
+//  Created by Natneal Asmelash on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift_UITests/Swift_UITests.swift
@@ -0,0 +1,16 @@
+//
+//  Swift_UITests.swift
+//  Swift_UITests
+//
+//  Created by Natneal Asmelash on 9/18/24.
+//
+
+import Testing
+
+struct Swift_UITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
